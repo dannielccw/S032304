5a3e39360ac918844e4c0000f99cdf239422e287
dannielccw <dannielccw@gmail.com>
2017-03-23 15:32:07 +0800

    V2

--- app/src/main/java/com/example/yvtc/s032304/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s032304/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.yvtc.s032304;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -15,13 +16,23 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout layout = new LinearLayout(MainActivity.this);
         layout.setOrientation(LinearLayout.VERTICAL);
 
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+
         Button btn = new Button(MainActivity.this);
         btn.setText("Click me");
+        btn.setLayoutParams(params);
         layout.addView(btn);
 
         Button btn2 = new Button(MainActivity.this);
         btn2.setText("Click me 2");
+        btn2.setLayoutParams(params);
         layout.addView(btn2);
+
+        Button btn3 = new Button(MainActivity.this);
+        btn3.setText("Click me 3");
+        btn3.setLayoutParams(params);
+        layout.addView(btn3);
+
 
         setContentView(layout);
     }
